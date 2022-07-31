4ba4dfed1962d3338e42f4703945073041747815
hassancodess <dev.hassancodes@gmail.com>
2022-07-31 17:57:15 +0500

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif'],
+    },
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        lightGreen: '#41cd52',
+        lightGray: '#f8f8f8',
+      },
+    },
+  },
+  plugins: [],
+}
